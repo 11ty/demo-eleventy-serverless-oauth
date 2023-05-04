40dd346172f91edfdc50714064a8f95b2c58b3b4
Zearin <Zearin@users.noreply.github.com>
2023-05-04 16:44:57 -0400

    Add JSDoc (for completion) .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 
+/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "dynamic",
